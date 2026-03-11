58a964896376e469e650450a6f612e3faca67406
Isabella Wiggins <belle@women-lovers.com>
2026-03-11 15:39:23 -0700

    Handle request and parse data

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
 import express from 'express';
+import { parseInput } from './parser.js';
 
 const app = express();
+app.use(express.json());
+
+app.post('/', (req, res) => {
+    res.sendStatus(200);
+    parseInput(req);
+});
+
 
 // Initialize the server
 export function startServer(port: number = 7355) {
